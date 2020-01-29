2f31f232bf0a93d4457fe1402dbbcc68d358e726
Prashanth <bdanalytics5@gmail.com>
2020-01-29 22:42:29 +0530

    Second File in project

--- file2.py
+++ file2.py
@@ -2,3 +2,8 @@ data = "Welcome to my website"
 file = open("hello.txt",'w')
 file.write(data)
 file.close()
+
+data = "\nwelcome to my website. This data is being amended to the hello.txt file"
+file = open("hello.txt", 'a')
+file.write(data)
+file.close()
